c5a73643998b66c98d1dccc1d480f92e43ad2123
qiuxan <qiuxan@gmail.com>
2024-07-24 08:06:18 +1000

    Add project files.

---
+++ Authentication/AppRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace RoleBasedAuthorizationDemo.Authentication
+{
+    public static class AppRoles
+    {
+        public const string Administrator = "Administrator";
+        public const string User = "User";
+        public const string VipUser = "VipUser";
+    }
+}
